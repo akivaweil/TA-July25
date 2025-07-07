cc593f1d2fb6d674611abde333e431e73a791ca1
akivaweil <akivaweil@gmail.com>
2025-07-07 10:40:46 -0700

    Before integrating custom ServoControl implementation

---
+++ include/ServoControl.h
@@ -0,0 +1,32 @@
+#ifndef SERVO_CONTROL_H
+#define SERVO_CONTROL_H
+
+#include <Arduino.h>
+
+class ServoControl {
+private:
+    int pin;
+    int channel;
+    int frequency;
+    int resolution;
+    int minPulseWidth;
+    int maxPulseWidth;
+    int minAngle;
+    int maxAngle;
+    
+    int angleToDuty(float angle);
+    
+public:
+    ServoControl();
+    
+    void init(int servoPin, int pwmChannel = 0, int freq = 50, int res = 16);
+    void write(float angle);
+    void writeMicroseconds(int microseconds);
+    void detach();
+    
+    // Setters for customization
+    void setPulseWidthRange(int minUs, int maxUs);
+    void setAngleRange(int minDeg, int maxDeg);
+};
+
+#endif 
